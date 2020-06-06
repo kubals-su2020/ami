b71556fdb0da37ac3ed186f978e121ea8ee4dbae
kubal-s <kubal.s@husky.neu.edu>
2020-06-06 15:04:08 -0400

    removed access keys

--- buildAmi.sh
+++ buildAmi.sh
@@ -1,6 +1,4 @@
 packer build \
-    -var 'aws_access_key=AKIAJSG4FNJOH4KPOFGA' \
-    -var 'aws_secret_key=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' \
     -var 'aws_region=us-east-1' \
     -var 'subnet_id=subnet-86965dd9' \
     -var 'source_ami=ami-068663a3c619dd892'\
